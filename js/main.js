1f9697480c1a8a72df3206be9c3dc080a108616f
Ashris <ashris.me@gmail.com>
2016-06-08 23:45:49 +0530

    Major Upgrades
    Apart from the database, pretty much everything is done. Mobile
    responsive check.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var yearval=2005;
-var bip;
+var bip=-1;
 var dat=[
   {
     "Name": "Kabhi Aisa Lagta Hai",
@@ -40,7 +40,7 @@ var dat=[
   {
     "Name": "Action Shoes Ad",
     "Link": "ax5Fl7huJZw",
-    "Year": 1999,
+    "Year": "any",
     "Tags": "Advertisement"
   },
   {
@@ -52,7 +52,7 @@ var dat=[
   {
     "Name": "Amul Ad",
     "Link": "tl7Lnb2ocC4",
-    "Year": 1997,
+    "Year": "any",
     "Tags": "Advertisement"
   },
   {
@@ -117,7 +117,7 @@ var dat=[
   },
   {
     "Name": "Kasauti Zindagi Kay Title",
-    "Link": "ooz-JaLzQjg",
+    "Link": "965dXJUU-w8",
     "Year": 2001,
     "Tags": "Serial"
   },
@@ -148,7 +148,7 @@ var dat=[
   {
     "Name": "Amul Ad 2",
     "Link": "RW9DSUbZX34",
-    "Year": 1996,
+    "Year": "any",
     "Tags": "Advertisement"
   },
   {
@@ -161,6 +161,468 @@ var dat=[
     "Name": "Chandu ke Chacha",
     "Link": "b9Ni-Bh488Y",
     "Year": 2004,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Gilli Danda Title",
+    "Link": "STlM8KCr5RU",
+    "Year": 2003,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Ramanand Sagar Krishna Title",
+    "Link": "WDz8cxqIL6g",
+    "Year": 1995,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Ramayana Title Track",
+    "Link": "R9g7kiK_cCg",
+    "Year": 1990,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Vikram Betal Song",
+    "Link": "uA8FzJGAieQ",
+    "Year": 1990,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Neele neele ambar par",
+    "Link": "nEnLt3pasxE",
+    "Year": 1983,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Voh chali voh chali",
+    "Link": "oZnSZnSpXA4",
+    "Year": 2000,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Aye Hip hopper",
+    "Link": "DCFrCX4HPO8",
+    "Year": 2005,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Mahabharata Title Track",
+    "Link": "d3qg7lw7tqc",
+    "Year": 1990,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Dekh Bhai Dekh Title",
+    "Link": "OnBUxBeKX2s",
+    "Year": 1993,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Ek Chidiya Anek Chidiya",
+    "Link": "EG9SCBd16lI",
+    "Year": 1991,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Malgudi Days Title Track",
+    "Link": "JQLYQ0GhBtE",
+    "Year": 1992,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Yeh ladka haye allah (new)",
+    "Link": "yRf2Jhq1XkU",
+    "Year": 2007,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Chura Liya Hai",
+    "Link": "-KgeYnMFBAU",
+    "Year": 1992,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Kya hua tera vada",
+    "Link": "xDbK1eZYVzg",
+    "Year": 1977,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Surbhi Title Track",
+    "Link": "5RcRzn6MdBs",
+    "Year": 1993,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Vijay Vishwa Tiranga Pyara",
+    "Link": "_BYqcN4K_Fo",
+    "Year": 1994,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Ae Mere Watan Ke Logo",
+    "Link": "YRC36g5",
+    "Year": "any",
+    "Tags": "Music"
+  },
+  {
+    "Name": "Jahan Daal Daal Par Sone Ki",
+    "Link": "-50qe9qrUDk",
+    "Year": "any",
+    "Tags": "Music"
+  },
+  {
+    "Name": "Dhaara Ad",
+    "Link": "hIDWHiTrjZ8",
+    "Year": "any",
+    "Tags": "Advertisement"
+  },
+  {
+    "Name": "Rasna Ad",
+    "Link": "ah0g0joiWi0",
+    "Year": "any",
+    "Tags": "Advertisement"
+  },
+  {
+    "Name": "Kaliyon ka chaman (new version )",
+    "Link": "xDonIFjYbFU",
+    "Year": 2000,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Titan Ad",
+    "Link": "lyzxItHM740",
+    "Year": "any",
+    "Tags": "Advertisement"
+  },
+  {
+    "Name": "Nirma Ad",
+    "Link": "CbNTcvzrpBY",
+    "Year": "any",
+    "Tags": "Advertisement"
+  },
+  {
+    "Name": "Lifebuoy Ad",
+    "Link": "LMojcd3KvRQ",
+    "Year": "any",
+    "Tags": "Advertisement"
+  },
+  {
+    "Name": "Paree Hoon Main",
+    "Link": "WjZf9VM4Lgc",
+    "Year": 1992,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Meri Neend",
+    "Link": "JSpSJmPI9q0",
+    "Year": 2002,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Purani Jeans aur Guitar",
+    "Link": "wbh8MkI9zo4",
+    "Year": 2005,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Bheegi Bheegi",
+    "Link": "NHg3eTI9VRU",
+    "Year": 2002,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Bolo Ta Ra Ra Ra",
+    "Link": "4Ay5OOxRTTE",
+    "Year": 1995,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Rangilo Mharo Dholna",
+    "Link": "edKJih4Wb3I",
+    "Year": 1999,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Sun Re Peepal",
+    "Link": "DjHG8d0vVvA",
+    "Year": 2002,
+    "Tags": "Music"
+  },
+  {
+    "Name": "sayonee",
+    "Link": "0GLYKYgSE0Y",
+    "Year": 1997,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Chana ve ghar aaja ve",
+    "Link": "ihYnXDzZL4g",
+    "Year": 2005,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Hawa mein udta jaye",
+    "Link": "O0lEfszxPic",
+    "Year": 2004,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Saiyaan Dil mein aana re (new)",
+    "Link": "G5y5adM2ZNo",
+    "Year": 2006,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Pehla Nasha",
+    "Link": "bRUzf7LgjLM",
+    "Year": 1992,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Main Koi Aisa Geet Gaun",
+    "Link": "3uxe_dAKjrM",
+    "Year": 1997,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Zara Nazaron se kehdo",
+    "Link": "Ly81vyWjFW4",
+    "Year": 2006,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Ye Haseen Vaadiyan",
+    "Link": "iHXs-bLYf00",
+    "Year": 1992,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Sandese Aate Hain",
+    "Link": "SUtAfV2_3Kw",
+    "Year": 1997,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Zara Zara Behekta Hai",
+    "Link": "UZWrltQLTzE",
+    "Year": 2001,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Sach Keh Raha Hai Deewana",
+    "Link": "S4t2y_t4g4k",
+    "Year": 2001,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Pokemon Hindi Title Song",
+    "Link": "vUFDHVAaji8",
+    "Year": 2003,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Tu",
+    "Link": "OVI6Uzh5Las",
+    "Year": 2005,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Main Deewana",
+    "Link": "C0BnxutSJ9k",
+    "Year": 2005,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Bob the Builder Hindi",
+    "Link": "15hQ-7L2Q9A",
+    "Year": "any",
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Jungle Book Song",
+    "Link": "iLK-iD1dQb8",
+    "Year": 2001,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Captain Planet",
+    "Link": "DMoodiD0UEI",
+    "Year": 2001,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Doraemon",
+    "Link": "b2jI2RtYfww",
+    "Year": 2004,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Baby Looney Tunes",
+    "Link": "epSR5uU00MI",
+    "Year": 2004,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Tera milna pal do pal ka",
+    "Link": "5TJ5-6lqLFw",
+    "Year": 2005,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Ab muje raat din",
+    "Link": "42bGoPIBDxg",
+    "Year": 1999,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Deewana Tera",
+    "Link": "J8zlroVMYeM",
+    "Year": 1999,
+    "Tags": "Music"
+  },
+  {
+    "Name": "The Addams Family",
+    "Link": "IokBKvFiCvM",
+    "Year": 2001,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Scooby Doo",
+    "Link": "0_C2HJvtRDY",
+    "Year": 2000,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Shinchan",
+    "Link": "X9R029JAI2Q",
+    "Year": 2004,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "School Chale Hum",
+    "Link": "BXWqRjTtiQU",
+    "Year": 2003,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Tum Chalo toh Hindustaan Chale",
+    "Link": "JR8i9p3pcPg",
+    "Year": 2004,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Tuta Tuta ek parinda",
+    "Link": "a--i6F3fNkw",
+    "Year": 2006,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Tera mera pyaar",
+    "Link": "ufJ4xXKLq5Q",
+    "Year": 2003,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Neend Churayi Meri",
+    "Link": "VWYrFlbMk40",
+    "Year": 1997,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Raadha Kaise na Jale",
+    "Link": "qNnvL0ztJhA",
+    "Year": 2001,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Chaiyya Chaiyya",
+    "Link": "VKK_JZJywqk",
+    "Year": 1998,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Mast",
+    "Link": "hHk9IMf92Pk",
+    "Year": 1999,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Son Pari Song",
+    "Link": "Z5Qwrk4v7mo",
+    "Year": 2003,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Kyunki Saas Bhi Kabhi Bahu Thi",
+    "Link": "zJqTH-s3e90",
+    "Year": 2002,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Kahani Ghar Ghar Ki",
+    "Link": "eIdXdscY_Vs",
+    "Year": 2003,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Kumkum",
+    "Link": "Uf9zGNgPbwc",
+    "Year": 2003,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Shagun",
+    "Link": "surBnqvbqrk",
+    "Year": 2004,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Left Right Left",
+    "Link": "T5M94dkE_Ek",
+    "Year": 2005,
+    "Tags": "Serial"
+  },
+  {
+    "Name": "Complan Ad",
+    "Link": "t-fysFHfBYM",
+    "Year": "any",
+    "Tags": "Advertisement"
+  },
+  {
+    "Name": "lagi tumse mann ki lagan",
+    "Link": "eIxF3ApNr5w",
+    "Year": 2003,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Tose Naina laage",
+    "Link": "5R1vsrGrRgs",
+    "Year": 2007,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Sajna aa bhi jaa",
+    "Link": "PGQn0_pqxlw",
+    "Year": 2002,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Zara zara behekta hai",
+    "Link": "sydT76PgUb0",
+    "Year": 2001,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Dil keh raha hai",
+    "Link": "0PuT3Wk-lfs",
+    "Year": 2002,
+    "Tags": "Music"
+  },
+  {
+    "Name": "Aa bhi jaa",
+    "Link": "hNfN2KfDORw",
+    "Year": 2002,
     "Tags": "Music"
   }
 ];
@@ -208,9 +670,22 @@ shuffle(dat);
     }
 
 function forshana(abra){
-	bip=abra;
+	if(bip==-1){
+$('.framee').html('<iframe id="ytplayer" type="text/html" src="" frameborder="0" allowfullscreen></iframe>');
+bip=100;
+}
 	var videoid=abra.getAttribute("damn");
 var dork="https://www.youtube.com/embed/"+videoid+"?autoplay=1&showinfo=0&controls=1";
 
+$('#ytplayer').attr('src', dork);
+}
+function yalgaar2(){
+	var randN=Math.floor(Math.random()*(dat.length-1));
+	var videoid=dat[randN].Link;
+	var dork="https://www.youtube.com/embed/"+videoid+"?autoplay=1&showinfo=0&controls=1";
+if(bip==-1){
+$('.framee').html('<iframe id="ytplayer" type="text/html" src="" frameborder="0" allowfullscreen></iframe>');
+bip=100;
+}
 $('#ytplayer').attr('src', dork);
 }
